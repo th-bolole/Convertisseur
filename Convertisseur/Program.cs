bac8a554cbe6337b629714432bd3a18489ae788d
ThB <171622408+th-bolole@users.noreply.github.com>
2026-06-17 13:32:50 +0200

    Ajoutez des fichiers projet.

---
+++ Convertisseur/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convertisseur
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Convertisseur d'unités
+            Console.WriteLine("Entrer le nombre de degés :");
+            double nbr = double.Parse(Console.ReadLine());
+            Celsius(nbr);
+        }
+        static double Celsius(double nbrSaisie)
+        {
+            Console.WriteLine(+nbrSaisie + " °C");
+            Fareneith(nbrSaisie);
+            return nbrSaisie;
+        }
+        static double Fareneith(double nbrFareneith)
+        {
+            nbrFareneith = (nbrFareneith*1.8 + 32);
+
+            Console.WriteLine(nbrFareneith + " °F");
+            return nbrFareneith;
+        }
+
+
+
+    }
+}
---
+++ Convertisseur/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("Convertisseur")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Convertisseur")]
+[assembly: AssemblyCopyright("Copyright © HP 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("a8441c1a-df8b-4ac9-b71d-cf66f0afd9b6")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire
+//      Numéro de build
+//      Révision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
